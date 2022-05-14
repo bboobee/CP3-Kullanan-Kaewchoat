48209129ee46360f412293716d548c2adce056f3
bboobee <kc.kullanan@gmail.com>
2022-05-14 22:27:07 +0700

    Created Exercise 4
    1st created

---
+++ Exercise4_Kullanan_K.py
@@ -0,0 +1,10 @@
+ENG = 92.3
+BUS = 73
+INC = 88.2
+COM = 90
+
+print("--- Your Score ---")
+print("Foundation English",":",ENG)
+print("General Business",":",BUS)
+print("Introduction to Computer Systems",":",INC)
+print("Computer Programming",":",COM)
